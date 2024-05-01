60f980af63ded63003e9a82386db58e68ace47e3
Gideon Mutai <giddymutai13@gmail.com>
2024-05-01 21:09:31 +0300

    Adds a state hook

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  // const [state, setState ] = useState('OFF')
+
+  // function changeState() {
+  //   setState((prevState => (prevState === 'OFF'? 'ON' : 'OFF')))
+  // }
+  // return <button onClick={changeState}>{state}</button>;
+
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "white";
+
+  return (
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "ON" : "OFF"}
+    </button>
+  );
 }
 
 export default Toggle;
